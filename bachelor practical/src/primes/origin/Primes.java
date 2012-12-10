3ba5be86d06c2fd90ee06f72c3e7c2a11442ac35
javaprogger <javaprogger@aol.com>
2012-12-10 14:01:37 +0100

    Primes implementiert

--- bachelor practical/src/primes/origin/Primes.java
+++ bachelor practical/src/primes/origin/Primes.java
@@ -1,5 +1,21 @@
 package primes.origin;
 
 public class Primes {
-
+	
+	public static boolean isPrime(int number){
+		double tmp = Math.sqrt(number);
+		
+		for (int i = 3; i < tmp; i+=2) {
+			if(number % i == 0){
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		int i = 23;
+		System.out.println(i + " is " + (Primes.isPrime(i) ? "" : "not ") + "a prime");
+	}
 }
